862218676a6e3ba27e9bbc5d5d22d587fb2c643f
esmerlinp <esmerlinep@gmail.com>
2025-12-07 18:56:42 -0400

    conexion con db postgresql

--- app/blueprints/usuarios.py
+++ app/blueprints/usuarios.py
@@ -1,13 +1,34 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+import psycopg2
+
 
 usuarios_bp = Blueprint('usuarios', __name__, url_prefix='/api')
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        host='ep-morning-wildflower-addmvwux.c-2.us-east-1.aws.neon.tech',
+        database='AKDMIA_CLIENTE',
+        user='neondb_owner',
+        password='npg_fFT1cLH8gjuy'
+    )
+
+
+
+
+@usuarios_bp.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM tblcomun')
+    usuarios = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(usuarios)
+
 
 @usuarios_bp.route('/perfil', methods=['GET'])
 def perfil():
     return 'Perfil de usuarios'
 
 
-@usuarios_bp.route('/perfil')
-def buscar():
-    query = request.args.get('q')
-    return f'Buscando: {query or "nada"}'
