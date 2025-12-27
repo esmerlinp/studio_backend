b497a500618bdb61695297a6dc1931588ca4d524
esmerlinp <esmerlinep@gmail.com>
2025-12-27 18:37:03 -0400

    feat: Actualizar configuración de ejecución para usar variable de entorno PORT y desactivar modo debug

--- run.py
+++ run.py
@@ -217,7 +217,7 @@ def confirmation_account():
 
 if __name__ == '__main__':
         
-    app.run(debug=True)
+    #app.run(debug=True)
     # Usa la variable de entorno PORT si existe, de lo contrario 8080
-    #port = int(os.environ.get("PORT", 8080))
-    #app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
